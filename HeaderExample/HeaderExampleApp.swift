a90b683651b96b00334065198ddaa8d8379a852b
Graeme Armstrong <garmstrong@lcs.on.ca>
2022-11-06 10:29:58 -0500

    Initial Commit

---
+++ HeaderExample/HeaderExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderExampleApp.swift
+//  HeaderExample
+//
+//  Created by Graeme Armstrong on 2022-11-06.
+//
+
+import SwiftUI
+
+@main
+struct HeaderExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
